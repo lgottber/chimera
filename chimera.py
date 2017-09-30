fa3a40eabfcb8cfa7e8189f5005e88239c6782b8
Lauren Gottberg <lgottber@uncc.edu>
2017-09-29 21:24:29 -0400

    initial commit

---
+++ chimera.py
@@ -0,0 +1,23 @@
+#imports
+from google import google, images
+
+#format image type
+def format():
+    options = images.ImageOptions()
+    options.image_type = images.ImageType.FACE
+    options.exact_width = 
+    options.exact_height =
+    
+
+#downloading pictures of x randomly
+def getRandomX(x):
+
+#get animal to sort for
+
+#search for that animal
+
+#get options
+
+#choose an option randomly
+
+#merging picture
